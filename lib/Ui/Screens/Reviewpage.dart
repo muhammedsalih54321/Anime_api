b1dc508c682f874dd8414adc093b37980d45a4f9
muhammedsalih54321 <muhammedsali54231@gmail.com>
2024-06-11 12:20:14 +0530

    updated

--- lib/Ui/Screens/Reviewpage.dart
+++ lib/Ui/Screens/Reviewpage.dart
@@ -3,53 +3,48 @@ import 'package:webview_flutter/webview_flutter.dart';
 
 class Webscreen extends StatefulWidget {
   final String videoUrl;
-  const Webscreen({super.key,required this.videoUrl});
+  const Webscreen({super.key, required this.videoUrl});
 
   @override
   State<Webscreen> createState() => _WebscreenState();
 }
 
 class _WebscreenState extends State<Webscreen> {
-     late WebViewController controller;
+  late WebViewController controller;
 
   @override
   void initState() {
     super.initState();
 
-  controller = WebViewController()
-  ..setJavaScriptMode(JavaScriptMode.unrestricted)
-  ..setBackgroundColor(const Color(0x00000000))
-  ..setNavigationDelegate(
-    NavigationDelegate(
-      onProgress: (int progress) {
-        // Update loading bar.
-      },
-      onPageStarted: (String url) {},
-      onPageFinished: (String url) {},
-      onHttpError: (HttpResponseError error) {},
-      onWebResourceError: (WebResourceError error) {},
-      onNavigationRequest: (NavigationRequest request) {
-        if (request.url.startsWith(widget.videoUrl)) {
-          return NavigationDecision.prevent;
-        }
-        return NavigationDecision.navigate;
-      },
-    ),
-  )
-  ..loadRequest(Uri.parse(widget.videoUrl));
-    
-  
+    controller = WebViewController()
+      ..setJavaScriptMode(JavaScriptMode.unrestricted)
+      ..setBackgroundColor(const Color(0x00000000))
+      ..setNavigationDelegate(
+        NavigationDelegate(
+          onProgress: (int progress) {
+            // Update loading bar.
+          },
+          onPageStarted: (String url) {},
+          onPageFinished: (String url) {},
+          onHttpError: (HttpResponseError error) {},
+          onWebResourceError: (WebResourceError error) {},
+          onNavigationRequest: (NavigationRequest request) {
+            if (request.url.startsWith(widget.videoUrl)) {
+              return NavigationDecision.prevent;
+            }
+            return NavigationDecision.navigate;
+          },
+        ),
+      )
+      ..loadRequest(Uri.parse(widget.videoUrl));
   }
+
   @override
   Widget build(BuildContext context) {
-    return Scaffold(backgroundColor: Colors.white,
-      appBar: AppBar(
-       
-        // This drop down menu demonstrates that Flutter widgets can be shown over the web view.
-        
-      ),
+    return Scaffold(
+      backgroundColor: Colors.white,
+      appBar: AppBar(),
       body: WebViewWidget(controller: controller),
-      
     );
   }
 }
